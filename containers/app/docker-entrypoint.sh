9e18517daec9bdbf6e62f822ec0630320dfe812a
Ohji Kashiwazaki <okashiwazaki@gsx.co.jp>
2017-12-25 15:14:45 +0900

    add check if DB_HOST variable exists.

--- containers/app/docker-entrypoint.sh
+++ containers/app/docker-entrypoint.sh
@@ -16,6 +16,20 @@ function wait_for_postgres () {
   printf "\n"  >&2
 }
 
+function print_usage () {
+  printf "Usage :\n" >&2
+  printf "$ cd /path/to/caras-framework/\n" >&2
+  printf "$ docker-compose run --rm app\n" >&2
+  printf "See https://github.com/gsx-lab/caras-framework/blob/master/docs/INSTALL.md#install-on-docker\n" >&2
+}
+
+# DB_HOST declared?
+if [ -z "$DB_HOST" ]; then
+  printf "Environment variable 'DB_HOST' must be declared\n" >&2
+  print_usage
+  exit 1
+fi
+
 # create config files if not exist.
 if [ ! -f ${APPDIR}/config/database.yml ]; then
   cp ${APPDIR}/config/database.yml.sample ${APPDIR}/config/database.yml
